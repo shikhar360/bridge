73931b2bdaf9eca82c6c5152c8c0dcb75c482351
Rahul Sethuram <rahul.eth@icloud.com>
2023-07-27 08:44:55 +0200

    feat: nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,61 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  RainbowKitProvider,
+  darkTheme,
+  getDefaultWallets,
+} from "@rainbow-me/rainbowkit";
+import { mainnet, polygon, arbitrum, bsc } from "wagmi/chains";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, polygon, arbitrum, bsc],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Zoomer",
+  projectId: "296a55745d9880bb16e1386b1b0eb360",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider
+      theme={extendTheme({
+        styles: {
+          global: () => ({
+            body: {
+              bg: "#FEFC52",
+            },
+          }),
+        },
+      })}
+    >
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider
+          coolMode
+          chains={chains}
+          theme={darkTheme({
+            accentColor: "#FEFC52",
+            accentColorForeground: "black",
+            borderRadius: "small",
+            fontStack: "system",
+            overlayBlur: "small",
+          })}
+        >
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ChakraProvider>
+  );
 }
